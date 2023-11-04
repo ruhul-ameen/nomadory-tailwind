648b8fd9ad451165ea34e8c7323aa9744be21244
ruhul <ruhulamin18753@gmail.com>
2023-11-05 02:21:00 +0600

    menu component done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: { min: "360px" },
+        // => @media (min-width: 640px) { ... }
+        sm: { min: "640px" },
+        // => @media (min-width: 640px) { ... }
+
+        md: { min: "768px" },
+        // => @media (min-width: 768px) { ... }
+
+        lg: { min: "1024px" },
+        // => @media (min-width: 1024px) { ... }
+
+        xl: { min: "1280px" },
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": { min: "1536px" },
+        // => @media (min-width: 1536px) { ... }
+        "3xl": { min: "1600px" },
+        // => @media (min-width: 1600px) { ... }
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "bg-menu": "url('../public/image/bg-menu.png')",
+        "bg-menu-tr": "url('../public/image/bg-menu-tr.png')",
+        "bg-menu-bl": "url('../public/image/bg-menu-bl.png')",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
